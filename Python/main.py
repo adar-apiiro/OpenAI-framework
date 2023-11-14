720650dee82ee3f4c6cf58f172802d4becbac738
adar-apiiro <144586914+adar-apiiro@users.noreply.github.com>
2023-11-14 09:42:30 +0200

    Update main.py

--- Python/main.py
+++ Python/main.py
@@ -1,21 +1,10 @@
+import numpy as np
 import openai
-# to get proper authentication, make sure to use a valid key that's listed in
-# the --api-keys flag. if no flag value is provided, the `api_key` will be ignored.
-openai.api_key = "EMPTY"
-openai.api_base = "http://localhost:8000/v1"
+from dotenv import load_dotenv
 
-model = "vicuna-7b-v1.5"
-prompt = "Once upon a time"
-
-# create a completion
-completion = openai.Completion.create(model=model, prompt=prompt, max_tokens=64)
-# print the completion
-print(prompt + completion.choices[0].text)
-
-# create a chat completion
-completion = openai.ChatCompletion.create(
-  model=model,
-  messages=[{"role": "user", "content": "Hello! What is your name?"}]
-)
-# print the completion
-print(completion.choices[0].message.content)
+def main():
+    # OPENAI-KEY
+    openai.api_key = os.getenv("OPENAI_API_KEY")
+    
+if __name__ == "__main__":
+    main()
